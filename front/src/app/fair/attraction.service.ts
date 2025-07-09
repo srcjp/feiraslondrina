9e0ffdad83de1ed034e90da7ba3d9569d48ec0c3
srcjp <113392601+srcjp@users.noreply.github.com>
2025-07-09 15:25:22 -0300

    Enable attractions management

--- front/src/app/fair/attraction.service.ts
+++ front/src/app/fair/attraction.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environmet/environment';
 
@@ -16,20 +16,25 @@ export class AttractionService {
 
   constructor(private http: HttpClient) {}
 
+  private get authOptions() {
+    const token = localStorage.getItem('accessToken');
+    return token ? { headers: new HttpHeaders({ Authorization: `Bearer ${token}` }) } : {};
+  }
+
   listByFair(fairId: number): Observable<Attraction[]> {
     return this.http.get<Attraction[]>(`${this.api}/fair/${fairId}`);
   }
 
   create(fairId: number, data: Attraction): Observable<Attraction> {
-    return this.http.post<Attraction>(`${this.api}/fair/${fairId}`, data);
+    return this.http.post<Attraction>(`${this.api}/fair/${fairId}`, data, this.authOptions);
   }
 
   update(id: number, data: Attraction): Observable<Attraction> {
-    return this.http.put<Attraction>(`${this.api}/${id}`, data);
+    return this.http.put<Attraction>(`${this.api}/${id}`, data, this.authOptions);
   }
 
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.api}/${id}`);
+    return this.http.delete<void>(`${this.api}/${id}`, this.authOptions);
   }
 
   get(id: number): Observable<Attraction> {
